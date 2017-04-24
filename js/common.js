4a2e67dec7b8f908ac4859a5940f50ad4e7ee2bc
VolodymyrNovokhatskyi <vnovohatskyy@yahoo.com>
2017-04-24 15:11:12 +0300

    syper posilki first commit

---
+++ js/common.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+	//navigation
+	$(".more").click(function(event) {
+    event.preventDefault();
+		$(".dropdownTop").slideToggle();
+	});
+
+	//carouselTop
+	$(".prev-slide").click(function(){
+    $("#myCarousel").carousel('prev');
+  });
+  $(".next-slide").click(function(){
+    $("#myCarousel").carousel('next');
+  });
+
+	//carouselMiddle
+	$(".prev-slide2").click(function(){
+    $("#myCarousel2").carousel('prev');
+  });
+  $(".next-slide2").click(function(){
+    $("#myCarousel2").carousel('next');
+  });
+
+	//radio buttons
+	$('.radioCell').click(function() {
+ 	$(this).children('input').prop('checked', true);
+  
+  $('.radioCell').removeClass('selected');
+  $(this).toggleClass('selected');
+  });
+});
